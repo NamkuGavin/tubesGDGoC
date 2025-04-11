6163fe27206bcda32f0d520d824f4edbd11940c6
edselspth <edselspth@gmail.com>
2025-04-11 14:02:17 +0700

    feat: [EL] adding button navigation

---
+++ lib/ui/navigation/buttonNav.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart' show GoogleFonts;
+import 'package:tubes_gdgoc/ui/navigation/menu/home.dart' show HomeScreen;
+import 'package:tubes_gdgoc/ui/navigation/menu/profile.dart' show ProfileScreen;
+import 'package:tubes_gdgoc/ui/navigation/menu/transaction.dart' show TransactionScreen;
+
+class ButtonNav extends StatefulWidget {
+  const ButtonNav({super.key});
+
+  @override
+  State<ButtonNav> createState() => _ButtonNavState();
+}
+
+class _ButtonNavState extends State<ButtonNav> {
+  int _currentIndex = 0;
+  final List<Widget> _children = [HomeScreen(), TransactionScreen(), ProfileScreen()];
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: _children[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: onBarTapped,
+        items: [
+          BottomNavigationBarItem(
+              icon:
+              Image.asset('assets/icon/nav_bar/home.png', width: size.width * 0.1),
+              activeIcon: Image.asset('assets/icon/nav_bar/home_active.png',
+                  width: size.width * 0.1, color: Color(0xFF5EC57E)),
+              label: 'Home'),
+          BottomNavigationBarItem(
+              icon: Image.asset('assets/icon/nav_bar/transaction.png',
+                  width: size.width * 0.1),
+              activeIcon: Image.asset('assets/icon/nav_bar/transaction_active.png',
+                  width: size.width * 0.1, color: Color(0xFF5EC57E)),
+              label: 'Transaksi'),
+          BottomNavigationBarItem(
+              icon: Image.asset('assets/icon/nav_bar/profile.png',
+                  width: size.width * 0.1),
+              activeIcon: Image.asset('assets/icon/nav_bar/profile_active.png',
+                  width: size.width * 0.1, color: Color(0xFF5EC57E)),
+              label: 'Profile'),
+        ],
+        type: BottomNavigationBarType.fixed,
+        selectedLabelStyle: GoogleFonts.inter(fontWeight: FontWeight.w600),
+        unselectedLabelStyle: GoogleFonts.inter(fontWeight: FontWeight.w500),
+        selectedFontSize: 12,
+        unselectedFontSize: 12,
+        selectedItemColor: Color(0xFF5EC57E),
+        showUnselectedLabels: true,
+        showSelectedLabels: true,
+        elevation:5,
+      ),
+    );
+  }
+
+  void onBarTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+}

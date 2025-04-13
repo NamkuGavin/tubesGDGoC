1bbd907aae2de1b018712a8d07e77c5c8435082a
NamkuGavin <m.gavin.aga10@gmail.com>
2025-04-13 11:54:41 +0700

    fix: [GA] state management income

--- lib/common/notifier.dart
+++ lib/common/notifier.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class AppState with ChangeNotifier {
   bool _isPasswordNotVisible = true;
   int _currentIndex = 0;
+  final _dateController = TextEditingController();
+  String _selectedCategory = "Gaji";
 
   bool get isPasswordNotVisible => _isPasswordNotVisible;
   int get currentIndex => _currentIndex;
+  TextEditingController get dateController => _dateController;
+  String get selectedCategory => _selectedCategory;
 
   void togglePassVisible() {
     _isPasswordNotVisible = !_isPasswordNotVisible;
@@ -14,6 +19,16 @@ class AppState with ChangeNotifier {
 
   void onBarTapped(int index) {
     _currentIndex = index;
+    notifyListeners();
+  }
+
+  void formatDateControll(DateTime? picked) {
+    _dateController.text = DateFormat('d MMM yyyy').format(picked!);
+    notifyListeners();
+  }
+
+  void changeCategory(String? value) {
+    _selectedCategory = value!;
     notifyListeners();
   }
 }
